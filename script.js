ba630615e2ed4970b9c87a66ecc5cfc5453d4a3a
shimaaaaa <shimaakhallaf507@gmail.com>
2024-02-22 11:11:12 +0200

    refactoring the code

--- script.js
+++ script.js
@@ -1,56 +1,59 @@
-"use strict";
 var hieghest = 0;
-document.querySelector("#highestscore").textContent = hieghest;
-function play() {
-  document.querySelector("#hint").textContent = "Start guessing...";
-  var score = 20;
-  document.querySelector("#score").textContent = score;
+var score = 20;
+var randomNum = Math.floor(Math.random() * 20) + 1;
+const setEnitials = () => {
+  score = 20;
+  randomNum = Math.floor(Math.random() * 20) + 1;
+  changeTextContent("#hint", "Start guessing...");
+  changeTextContent("#score", score);
+  changeTextContent("#realNumber", "?");
+  changeTextContent("#highestscore", hieghest);
   document.querySelector("body").style.backgroundColor = "black";
   document.querySelector("#guessednumber").style.backgroundColor = "black";
-  document.querySelector("#realNumber").textContent = "?";
   document.querySelector("#check").addEventListener("click", guessTheNumber);
   document.querySelector("#guessednumber").value = "";
   document.querySelector("#guessednumber").disabled = false;
-  var randomNum = Math.floor(Math.random() * 20) + 1;
-  console.log(randomNum);
-  function guessTheNumber(event) {
-    if (score > 0) {
-      let gussedNum = document.querySelector("#guessednumber").value;
-      if (!gussedNum) {
-        document.querySelector("#hint").textContent = "😥 No number!";
-      } else if (gussedNum > randomNum) {
-        document.querySelector("#hint").textContent = "😥 Too High !";
-        score -= 1;
-        document.querySelector("#score").textContent = score;
-      } else if (gussedNum < randomNum) {
-        document.querySelector("#hint").textContent = "😥 Too low !";
-        score -= 1;
-        document.querySelector("#score").textContent = score;
-      } else {
-        document.querySelector("#hint").textContent = "🎉 Correct Number !";
-        document.querySelector("body").style.backgroundColor =
-          "rgb(83, 163, 83)";
-        document.querySelector("#guessednumber").style.backgroundColor =
-          "rgb(83, 163, 83)";
-        document.querySelector("#realNumber").textContent = randomNum;
-        document
-          .querySelector("#check")
-          .removeEventListener("click", guessTheNumber);
-        document.querySelector("#guessednumber").disabled = true;
-        if (score > hieghest) {
-          hieghest = score;
-          document.querySelector("#highestscore").textContent = hieghest;
-        }
-      }
+};
+const changeTextContent = (selector, text) => {
+  document.querySelector(`${selector}`).textContent = text;
+};
+const playAgain = (event) => {
+  setEnitials();
+};
+function guessTheNumber(event) {
+  if (score > 0) {
+    let gussedNum = Number(document.querySelector("#guessednumber").value);
+    if (!gussedNum) {
+      changeTextContent("#hint", "😥 No number!");
+    } else if (gussedNum !== randomNum) {
+      gussedNum > randomNum
+        ? changeTextContent("#hint", "😥 Too High !")
+        : changeTextContent("#hint", "😥 Too low !");
+      score -= 1;
+      document.querySelector("#score").textContent = score;
     } else {
-      document.querySelector("#hint").textContent = "😭 Game Over!";
+      changeTextContent("#hint", "🎉 Correct Number !");
+      document.querySelector("body").style.backgroundColor = "rgb(83, 163, 83)";
+      document.querySelector("#guessednumber").style.backgroundColor =
+        "rgb(83, 163, 83)";
+      changeTextContent("#realNumber", randomNum);
       document
         .querySelector("#check")
         .removeEventListener("click", guessTheNumber);
       document.querySelector("#guessednumber").disabled = true;
+      if (score > hieghest) {
+        hieghest = score;
+        changeTextContent("#highestscore", hieghest);
+      }
     }
+  } else {
+    changeTextContent("#hint", "😭 Game Over!");
+    document
+      .querySelector("#check")
+      .removeEventListener("click", guessTheNumber);
+    document.querySelector("#guessednumber").disabled = true;
   }
-  document.querySelector("#check").addEventListener("click", guessTheNumber);
 }
-play();
-document.querySelector("#again").addEventListener("click", play);
+setEnitials();
+document.querySelector("#check").addEventListener("click", guessTheNumber);
+document.querySelector("#again").addEventListener("click", playAgain);
